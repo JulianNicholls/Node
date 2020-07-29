2144d7dfbf63cff3ba9249a5671774d596c1ee88
Julian Nicholls <juliannicholls29@gmail.com>
2020-07-29 17:14:14 +0100

    Add comments ro zeller.

--- zeller.js
+++ zeller.js
@@ -12,9 +12,9 @@ const zDays = [
 
 // Algorithm here: https://en.wikipedia.org/wiki/Zeller%27s_congruence
 // 0 = Saturday, 1 = Sunday etc
-function zeller(day, month, year) {
-  const m = month < 3 ? month + 12 : month;
-  const y = month < 3 ? year - 1 : year;
+function zeller(day, month, year) { // 0..31, 1..12, full year
+  const m = month < 3 ? month + 12 : month;   // First 2 months of a year...
+  const y = month < 3 ? year - 1 : year;      // Work as 13 & 14 of the previous year
   const leap = Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400);
 
   const h = day + Math.floor((13 * (m + 1)) / 5) + y + leap;
@@ -22,6 +22,9 @@ function zeller(day, month, year) {
   return h % 7;
 }
 
+// Same, but for Julian Dates.
+// Many countries changed beginning 4/10/1582-15/10/1582.
+// UK changed 02/09/1752-14/09/1752
 function zellerJulian(day, month, year) {
   const m = month < 3 ? month + 12 : month;
   const y = month < 3 ? year - 1 : year;
@@ -45,7 +48,6 @@ const tests = [
 ];
 
 for (let item of tests) {
-  const z1 = zeller(...item);
   const z2 = zeller(...item);
 
   console.log(
@@ -53,11 +55,9 @@ for (let item of tests) {
       zDays[z2]
     }`
   );
-
-  if (z1 !== z2) console.error("They don't agree:", { z1, z2 });
 }
 
-// Check 1752
+// Check 1752 (UK centric)
 let z2 = zellerJulian(1, 1, 1752);
 console.log(`\n01/01/1752 (Julian): ${z2} - ${zDays[z2]}`);
 
@@ -65,4 +65,4 @@ z2 = zellerJulian(2, 9, 1752);
 console.log(`02/09/1752 (Julian): ${z2} - ${zDays[z2]}`);
 
 z2 = zeller(14, 9, 1752);
-console.log(`14/9/1752: ${z2} - ${zDays[z2]}`);
+console.log(`14/09/1752: ${z2} - ${zDays[z2]}`);
