b30bd841198f970f0faa8946beaaf56ede6e17c6
Julian Nicholls <juliannicholls29@gmail.com>
2021-09-10 16:45:58 +0100

    Add explicit nulls to test-map

--- test-map.js
+++ test-map.js
@@ -4,8 +4,15 @@ console.log(numbers);
 
 // Conditionally returning an entry puts undefined
 // in for the rest.
-const mapped = numbers.map(num => {
-  if(num % 3 == 0) return num;
+const mapped = numbers.map((num) => {
+  if (num % 3 == 0) return num;
 });
 
 console.log(mapped);
+
+const mappedNull = numbers.map((num) => {
+  if (num % 3 == 0) return num;
+  return null;
+});
+
+console.log(mappedNull);
