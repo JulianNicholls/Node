ef39e44d2f901680f5219551be330f1f6bd8f60f
Julian Nicholls <juliannicholls29@gmail.com>
2021-09-10 16:44:55 +0100

    Add timsort to sorts.

--- sorts.js
+++ sorts.js
@@ -5,10 +5,94 @@ const { formatns } = require('./times');
 const RANDOM = 0;
 const SORTED = 1;
 const REVERSE = 2;
+const MIN_MERGE = 32;
 
 const TYPES = ['Random', 'Already Sorted', 'Reverse Order'];
 
 let testarray;
+
+function timsort(a) {
+  const minRunLength = (n) => {
+    let r = 0;
+
+    while (n >= MIN_MERGE) {
+      r |= n & 1;
+      n >>= 1;
+    }
+
+    return n + r;
+  };
+
+  const insertionsort = (a, left, right) => {
+    for (let i = left + 1; i <= right; ++i) {
+      const temp = a[i];
+      let j = i - 1;
+
+      while (j >= left && a[j] > temp) {
+        a[j + 1] = a[j];
+        --j;
+      }
+
+      a[j + 1] = temp;
+    }
+  };
+
+  const merge = (a, l, m, r) => {
+    const len1 = m - l + 1;
+    const len2 = r - m;
+    const left = new Array(len1);
+    const right = new Array(len2);
+
+    for (let x = 0; x < len1; ++x) left[x] = a[l + x];
+
+    for (let x = 0; x < len2; ++x) right[x] = a[m + 1 + x];
+
+    let i = 0;
+    let j = 0;
+    let k = l;
+
+    while (i < len1 && j < len2) {
+      if (left[i] <= right[j]) {
+        a[k] = left[i];
+        ++i;
+      } else {
+        a[k] = right[j];
+        ++j;
+      }
+
+      ++k;
+    }
+
+    while (i < len1) {
+      a[k] = left[i];
+      ++k;
+      ++i;
+    }
+
+    while (j < len2) {
+      a[k] = right[j];
+      ++k;
+      ++j;
+    }
+  };
+
+  const n = a.length;
+
+  let minRun = minRunLength(MIN_MERGE);
+
+  for (let i = 0; i < n; i += minRun) {
+    insertionsort(a, i, Math.min(i + MIN_MERGE + 1, n - 1));
+  }
+
+  for (let size = minRun; size < n; size *= 2) {
+    for (let left = 0; left < n; left += 2 * size) {
+      const mid = left + size - 1;
+      const right = Math.min(left + 2 * size - 1, n - 1);
+
+      if (mid < right) merge(a, left, mid, right);
+    }
+  }
+}
 
 function shellsort(a) {
   const len = a.length;
@@ -162,25 +246,31 @@ function fullTest(sizes = 8) {
     console.log(`\n\t${TYPES[type]}`);
     let size = 100_000;
 
-    console.log('            ShellSort  MergeSort  QuickSort  JS sort()');
+    console.log('            ShellSort  MergeSort  QuickSort    TimSort  JS sort()');
 
     for (let i = 0; i < sizes; ++i) {
       testarray = newArray(size, type);
       let start = process.hrtime.bigint();
-      mergesort(testarray);
-      const mergeEnd = process.hrtime.bigint() - start;
-      checkSorted(testarray);
-
-      testarray = newArray(size, type);
-      start = process.hrtime.bigint();
       shellsort(testarray);
       const shellEnd = process.hrtime.bigint() - start;
       checkSorted(testarray);
 
       testarray = newArray(size, type);
       start = process.hrtime.bigint();
+      mergesort(testarray);
+      const mergeEnd = process.hrtime.bigint() - start;
+      checkSorted(testarray);
+
+      testarray = newArray(size, type);
+      start = process.hrtime.bigint();
       quicksort(testarray, 0, testarray.length - 1);
       const q2End = process.hrtime.bigint() - start;
+      checkSorted(testarray);
+
+      testarray = newArray(size, type);
+      start = process.hrtime.bigint();
+      timsort(testarray);
+      const timEnd = process.hrtime.bigint() - start;
       checkSorted(testarray);
 
       testarray = newArray(size, type);
@@ -198,7 +288,10 @@ function fullTest(sizes = 8) {
         `${rjust(size.toString(), 9)}: ${formatns(shellEnd, 9)}  ${formatns(
           mergeEnd,
           9
-        )}  ${formatns(q2End, 9)}  ${formatns(sortEnd, 9)}    Fastest: ${min}`
+        )}  ${formatns(q2End, 9)}  ${formatns(timEnd, 9)}  ${formatns(
+          sortEnd,
+          9
+        )}    Fastest: ${min}`
       );
 
       size *= coeffs[i % 3];
