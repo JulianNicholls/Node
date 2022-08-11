db1f55e9e22f1c0dde41047a1f1aa582fac39c58
Julian Nicholls <juliannicholls29@gmail.com>
2022-08-11 19:45:51 +0100

    Add capability to test against another hash in crypt

--- crypt.js
+++ crypt.js
@@ -1,9 +1,10 @@
 const bcrypt = require('bcryptjs');
 
-const password = process.argv[2];
+const [, , password, otherHash] = process.argv;
 
 const salt = bcrypt.genSaltSync(10);
 const hash = bcrypt.hashSync(password, salt);
 
 console.log(password, '=>', hash);
 console.log('OK:', bcrypt.compareSync(password, hash));
+if (otherHash) console.log(`${otherHash} OK:`, bcrypt.compareSync(password, otherHash));
