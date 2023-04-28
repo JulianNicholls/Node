62a23908c9c4cfe3904bd6e1b7410a79c60a6b17
Julian Nicholls <juliannicholls29@gmail.com>
2023-04-28 16:02:03 +0100

    Dhow the internet speed from the CSV

---
+++ read-speed.js
@@ -0,0 +1,64 @@
+const { parseFile } = require('@fast-csv/parse');
+const { ljust, rjust } = require('justify-text');
+
+const records = [];
+
+const adjustedRecord = (row) => {
+  const adjustedRow = { ...row };
+
+  adjustedRow.Timestamp = new Date(adjustedRow.Timestamp);
+  adjustedRow.Distance = Number(adjustedRow.Distance);
+  adjustedRow.Ping = Number(adjustedRow.Ping);
+  adjustedRow.Download = Number(adjustedRow.Download);
+  adjustedRow.Upload = Number(adjustedRow.Upload);
+
+  for (const fieldName of Object.keys(adjustedRow)) {
+    const unspaced = fieldName.replace(/ /g, '');
+
+    if (unspaced !== fieldName) {
+      adjustedRow[unspaced] = adjustedRow[fieldName];
+      delete adjustedRow[fieldName];
+    }
+  }
+
+  return adjustedRow;
+};
+
+function normalised(value) {
+  return (value / 1_000_000).toFixed(1);
+}
+
+function collectRecord(row) {
+  records.push(adjustedRecord(row));
+}
+
+function processRecords() {
+  for (const rec of records) {
+    console.log(
+      `${rec.Timestamp.toLocaleDateString('en-GB', {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric',
+      })}:  ${ljust(rec.Sponsor, 30)}  ${ljust(rec.ServerName, 20)}  ${rjust(
+        Math.round(rec.Distance),
+        3
+      )}Km ${rjust(rec.Ping.toFixed(1), 5)}ms ${rjust(normalised(rec.Download), 5)}MBit/s ${rjust(
+        normalised(rec.Upload),
+        5
+      )}MBit/s`
+    );
+  }
+}
+
+function main(filename) {
+  parseFile(filename, { headers: true })
+    .on('error', (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .on('end', processRecords)
+    .on('data', collectRecord);
+}
+
+const [, , filename = '/Users/julian/speedtest.csv'] = process.argv;
+main(filename);
