1202d1861081b7470c79a588edc7ff5ac9969e24
Julian Nicholls <juliannicholls29@gmail.com>
2023-09-29 20:51:31 +0100

    Update 2fa and show more of EXIF data

--- exifdata.js
+++ exifdata.js
@@ -1,21 +1,35 @@
 const ExifReader = require('exifreader');
 
 const interestingTags = [
+  'GPSAltitude',
   'GPSLatitude',
   'GPSLongitude',
   'GPSImgDirection',
   'GPSDestBearing',
   'GPSDateStamp',
+  'DateCreated',
+  'Make',
+  'Model',
 ];
 
-async function main(filename) {
-  const tags = await ExifReader.load(filename);
+async function main(filename, all) {
+  try {
+    const tags = await ExifReader.load(filename);
+    const tagList = all ? Object.keys(tags).sort() : interestingTags;
 
-  for (const name of interestingTags) {
-    console.log({ [name]: JSON.stringify(tags[name]) });
+    for (const name of tagList) {
+      console.log({ [name]: JSON.stringify(tags[name]) });
+    }
+
+    if (all === 'json') {
+      console.log('Stringified:\n');
+      console.log(JSON.stringify(tags));
+    }
+  } catch (err) {
+    console.error(err);
   }
 }
 
-const [, , filename = '/Users/julian/Pictures/IMG_0709.HEIC'] = process.argv;
+const [, , filename = '/Users/julian/Pictures/IMG_0709.HEIC', all = ''] = process.argv;
 
-main(filename);
+main(filename, all);
